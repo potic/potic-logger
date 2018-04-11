3b5c08e57b32ed9fcfebdb7622de5eb504cd30a7
Yaroslav Yermilov <yermilov17@gmail.com>
2018-04-12 02:19:14 +0300

    improve deploy script

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -14,14 +14,17 @@ warn ( ) {
 warn "Currently running docker images"
 docker ps -a
 
-warn "Killing currently running docker image..."
-docker kill potic-logger; docker rm potic-logger
-
 warn "Pulling latest docker image..."
 docker pull potic/potic-logger:$TAG_TO_DEPLOY
 
+warn "Killing currently running docker image..."
+docker kill potic-logger; docker rm potic-logger
+
 warn "Starting docker image..."
 docker run -dit --name potic-logger --restart on-failure --link potic-users -e LOG_PATH=/mnt/logs -v /mnt/logs:/mnt/logs -e LOGZIO_TOKEN=$LOGZIO_TOKEN -p 40410:8080 potic/potic-logger:$TAG_TO_DEPLOY
+
+warn "Wait 30sec to check status"
+sleep 30
 
 warn "Currently running docker images"
 docker ps -a
